11b75c4709860cef9b446a4bcab70ad48f103c83
Killian-Pro <cluzel.killian@gmail.com>
2020-06-14 23:51:38 +0200

    ajout de la recherche par key

--- JS/recherche.js
+++ JS/recherche.js
@@ -1,7 +1,14 @@
 $(document).keypress(function (e) {
+    /* KEYS */
+    var js = ["POP", "SPLICE", "PUSH", "SHIFT", "NPM", "EXPO", "REACT", "JS"];
+    var C = ["C", "C++"];
+    var Soft = ["SOFTWARE", "LOGICIEL", "SOFT", "DOWNLOAD"];
+    var kiki = ["KIKI", "KILLIAN", "CLUZEL", "KIP"];
+
+
+    /* DEBUT DE LA RECHECHE */
     var keycode = (e.keyCode ? e.keyCode : e.which);
     var input = document.getElementById("myInput");
-    console.log(input);
     if (input != null) { var filter = input.value.toUpperCase(); }
     if (keycode == '13') {
         switch (filter) {
@@ -10,9 +17,6 @@ $(document).keypress(function (e) {
                 break;
             case 'HTML':
                 document.location.href = "./pages/html.html";
-                break;
-            case 'JS':
-                document.location.href = "./pages/js.html";
                 break;
             case 'PHP':
                 document.location.href = "./pages/php.html";
@@ -23,17 +27,29 @@ $(document).keypress(function (e) {
             case 'PYTHON':
                 document.location.href = "./pages/python.html";
                 break;
-            case 'SOFTWARE':
+            case lire(Soft,filter):
                 document.location.href = "./pages/soft.html";
                 break;
-            case 'C':
+            case lire(js,filter):
+                document.location.href = "./pages/js.html";
+                break;
+            case lire(C,filter):
                 document.location.href = "./pages/c.html";
                 break;
-            case 'C++':
-                document.location.href = "./pages/c.html";
+            case lire(kiki,filter):
+                document.location.href = "https://www.cluzel-killian.fr/";
                 break;
             default:
                 document.location.href = "#";
         }
     }
 });
+
+function lire(tab,valueR) {
+    for (var i = 0; i < tab.length; i++)
+    if (tab[i] == valueR) {
+        var chaine = tab[i];
+    }
+    console.log(chaine);
+    return chaine;
+}
