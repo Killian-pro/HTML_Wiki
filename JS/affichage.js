ef2be33833324c2604c54b4e4379a36f652e4256
kip <cluzel.killian@gmail.com>
2020-07-20 09:14:25 +0200

    correct js and add black css

--- JS/affichage.js
+++ JS/affichage.js
@@ -1,25 +1,26 @@
 function masquer_div1() {
-	if (document.getElementById(1).style.display == 'none') {
-		document.getElementById(1).style.display = 'block';
+	console.log("ok");
+	if (document.getElementById(1).style.display == 'block') {
+		document.getElementById(1).style.display = 'none';
 	}
 	else {
-		document.getElementById(1).style.display = 'none';
+		document.getElementById(1).style.display = 'block';
 	}
 }
 function masquer_div2() {
-	if (document.getElementById(2).style.display == 'none') {
-		document.getElementById(2).style.display = 'block';
+	if (document.getElementById(2).style.display == 'block') {
+		document.getElementById(2).style.display = 'none';
 	}
 	else {
-		document.getElementById(2).style.display = 'none';
+		document.getElementById(2).style.display = 'block';
 	}
 }
 function masquer_div3() {
-	if (document.getElementById(3).style.display == 'none') {
-		document.getElementById(3).style.display = 'block';
+	if (document.getElementById(3).style.display == 'block') {
+		document.getElementById(3).style.display = 'none';
 	}
 	else {
-		document.getElementById(3).style.display = 'none';
+		document.getElementById(3).style.display = 'block	';
 	}
 }
 
